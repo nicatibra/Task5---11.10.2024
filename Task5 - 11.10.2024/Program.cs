92428b6d2e8617d89b9271b89674c88e44acd804
nicatibra <nicatibrahimlibusiness@gmail.com>
2024-10-11 10:59:08 +0400

    AddedUserInput

--- Task5 - 11.10.2024/Program.cs
+++ Task5 - 11.10.2024/Program.cs
@@ -8,6 +8,7 @@ namespace Task5___11._10._2024
     {
         static void Main(string[] args)
         {
+
             int[] arrNumbers = { 1, 2, 10 };
             CustomResize(ref arrNumbers, 4, 5, 6, 7, 8, 9);
 
@@ -16,6 +17,30 @@ namespace Task5___11._10._2024
             {
                 Console.WriteLine(arrNumbers[i]);
             }
+
+            //#region UserInputedVersion 
+            ////yazandan sonra bildim ki hec params-a ehtiyac olmur.
+            //int[] arrNumbers = { 1, 2, 10 };
+
+            //Console.WriteLine("How many new numbers do you want to enter?: ");
+            //int sizeOfArray = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Enter the numbers: ");
+            //int[] newNumbers = new int[sizeOfArray];
+            //for (int i = 0; i < sizeOfArray; i++)
+            //{
+            //    newNumbers[i] = Convert.ToInt32(Console.ReadLine());
+            //}
+            //Console.WriteLine(" ");
+
+            //CustomResize(ref arrNumbers, newNumbers);
+            //Console.WriteLine("Resized array: ");
+            //for (int i = 0; i < arrNumbers.Length; i++)
+            //{
+            //    Console.WriteLine(arrNumbers[i]);
+            //}
+            //#endregion
+
         }
 
         public static void CustomResize(ref int[] array, params int[] nums)
